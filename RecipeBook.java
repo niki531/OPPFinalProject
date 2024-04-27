f6b23880b8a3ddd65c2ed3a350f3af09861daf90
niki531 <137993765+niki531@users.noreply.github.com>
2024-04-27 17:49:28 -0400

    Update RecipeBook.java

--- RecipeBook.java
+++ RecipeBook.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
 
+public class RecipeBook {
+    private ArrayList<Recipe> recipeList;
+
+    public RecipeBook() {
+        this.recipeList = new ArrayList<>();
+    }
+
+    public void addRecipe(String recipeDetails) {
+        String[] parts = recipeDetails.split(";");
+        Recipe r = new Recipe(parts[0].trim()); 
+        for (int i = 1; i < parts.length; i++) {
+            r.addIngredient(parts[i].trim()); 
+        }
+        recipeList.add(r);
+    }
+    
+    public ArrayList<Recipe> getRecipes() {
+        return recipeList;
+    } 
+}
