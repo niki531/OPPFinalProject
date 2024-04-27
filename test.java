a65df9c3df83988b5f7ae157129aba00a0ca8e72
1715357606 <70891004+1715357606@users.noreply.github.com>
2024-04-27 17:06:55 +0000

    test2

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 
 public class test{
     public static void main(String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello World\n");
+        System.out.print("Hello World 2");
     }
 }
