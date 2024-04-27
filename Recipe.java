b6c9cc0023b346630cbebf7d9847ef0f42e56dc2
1715357606 <70891004+1715357606@users.noreply.github.com>
2024-04-27 16:16:46 -0400

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -1,1 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Recipe {
+    private String name;
+    private ArrayList<String> ingredientList;
+
+    public Recipe(String name) {
+        this.name = name;
+        this.ingredientList = new ArrayList<>();
+    }
+
+    public void addIngredient(String ingredient) {
+        ingredientList.add(ingredient);
+    }
+
+    public String displayRecipe() {
+      	StringBuilder sb = new StringBuilder();
+        sb.append("Recipe for: ").append(name).append("\n");
+        for (String ingredient : ingredientList) {
+            sb.append("- ").append(ingredient).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String displayPartRecipe() {
+    	StringBuilder sb = new StringBuilder();
+        sb.append("Partial Recipe for: ").append(name).append("\n");
+        Random rand = new Random();
+        for (String ingredient : ingredientList) {
+            if (rand.nextInt(10) < 2) {
+                sb.append("- ").append("\n"); // Just a dash if random is less than 2
+            } else {
+                sb.append("- ").append(ingredient).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+    
+}
