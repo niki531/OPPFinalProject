61961430a026cf14a0a016f98a4d3b624de787a8
niki531 <137993765+niki531@users.noreply.github.com>
2024-04-27 16:57:19 -0400

    Update SucceedGUI.java

---
+++ SucceedGUI.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SucceedGUI extends JFrame {
+    private JTextArea txtSucceed;
+    private JButton btnNextLevel;
+    private JButton btnExit;
+    private int currentLevel;
+
+    public SucceedGUI(int currentLevel) {
+        this.currentLevel = currentLevel;
+        setTitle("Level "+currentLevel+" Passed");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        txtSucceed = new JTextArea();
+        txtSucceed.setText("Congratulations! You passed level " + currentLevel + ".");
+        txtSucceed.setEditable(false);
+        txtSucceed.setMargin(new Insets(10, 30, 10, 30)); 
+        add(txtSucceed, BorderLayout.CENTER);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        btnNextLevel = new JButton("Next Level");
+        btnNextLevel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                nextClicked();
+            }
+        });
+        panel.add(btnNextLevel);
+
+        btnExit = new JButton("Exit");
+        btnExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                exitClicked();
+            }
+        });
+        panel.add(btnExit);
+
+        add(panel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    public void nextClicked() {
+        if (currentLevel== 1){
+            new Level2GUI();
+        }
+    }
+
+    public void exitClicked() {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        new SucceedGUI(1); 
+    }
+}
