ed1bf2810d4cbb571386526a232ee12e5012f8c2
niki531 <137993765+niki531@users.noreply.github.com>
2024-04-29 01:08:16 -0400

    Update RecipeBook.java

--- RecipeBook.java
+++ RecipeBook.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.SwingUtilities;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class RecipeBook {
     private ArrayList<Recipe> recipeList;
 
@@ -10,8 +16,8 @@ public class RecipeBook {
 
     public void addRecipe(String recipeDetails) {
         String[] parts = recipeDetails.split(";");
-        Recipe r = new Recipe(parts[0].trim()); 
-        for (int i = 1; i < parts.length; i++) {
+        Recipe r = new Recipe(parts[0].trim(), Integer.parseInt(parts[1].trim())); 
+        for (int i = 2; i < parts.length; i++) {
             r.addIngredient(parts[i].trim()); 
         }
         recipeList.add(r);
@@ -20,4 +26,18 @@ public class RecipeBook {
     public ArrayList<Recipe> getRecipes() {
         return recipeList;
     } 
+    
+    public void initializeRecipeBook(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                addRecipe(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Cannot reading from file: " + filename);
+            e.printStackTrace();
+        }
+    }
+
+    
 }
