16716208ddfe5e11994e0a4b517a4e0af44e9440
niki531 <137993765+niki531@users.noreply.github.com>
2024-04-29 00:10:11 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,12 +1,15 @@
+import java.util.Random;
 public class Customer {
     private int priceNTip;
     private String name;
     private Recipe order;
+    private Random random = new Random();
 
-    public Customer(String name, Recipe order) {
+    public Customer(String name, Recipe order, int price) {
         this.name = name;
         this.order = order;
-        this.priceNTip = 30; 
+        int rd = random.nextInt(30);
+        this.priceNTip = (int)price *(1+ rd/100); 
     }
 
     public int compareNPay(Recipe recipe) {
