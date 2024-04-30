2b400b1db8f77feff5855f3f6ff44da3fe94cdf2
niki531 <137993765+niki531@users.noreply.github.com>
2024-04-30 03:17:22 -0400

    Update Level1GUI.java

--- Level1GUI.java
+++ Level1GUI.java
@@ -7,83 +7,433 @@ public class Level1GUI extends JFrame {
     private Recipe product;
     private Customer customer1; 
     private RecipeBook recipebook;
-    private int aimMoney;
-    private int gainMoney;
-    private int checktimes;
-    private int level1Time;
-
+    private int aimMoney=15;
+    private int gainMoney=0;
+    private int checktimes=0;
+    private JTextArea currentProduct;
+    private JTextArea aimArea;
+    private JTextArea gainArea;
+    private JTextArea timeArea;
     private JButton btnCustomer1;
     private JButton btnRecipe;
-    private JButton btnIngredient1;
-    private JButton btnIngredient2;
-    private JButton btnIngredient3;
+    private JButton btnIce;
+    private JButton btnGin;
+    private JButton btnDryVermouth;
+    private JButton btnOrangeBitters;
+    private JButton btnMintLeaves;
+    private JButton btnLimeWedge;
+    private JButton btnSugar;
+    private JButton btnWhiteRum;
+    private JButton btnClubSoda;
+    private JButton btnTequila;
+    private JButton btnLimeJuice;
+    private JButton btnTripleSec;
+    private JButton btnBourbonWhiskey;
+    private JButton btnLemonJuice;
+    private JButton btnSimpleSyrup;
+    private JButton btnEggWhite;
+    private JButton btnSalt;
 
-    public Level1GUI() {
-       
-        btnCustomer1 = new JButton("Customer 1");
-        btnRecipe = new JButton("Recipe");
-        btnIngredient1 = new JButton("Ingredient 1");
-        btnIngredient2 = new JButton("Ingredient 2");
-        btnIngredient3 = new JButton("Ingredient 3");
+    private JButton btnMuddle;
+    private JButton btnShake;
+    private JButton btnStir;
+    private JButton btnRim;
+    private JButton btnStrain;
 
-        this.product = new Recipe("Product");
-        this.customer1 = new Customer("Alice", recipebook.get(0));
+    private JButton btnLemonTwist;
+    private JButton btnOrangeSlice;
+    private JButton btnCherry;
+    private int levelTime=10000;
+    private JLabel label;
+    private Timer timer;
+    private int count = 0;
+    private int currentLevel=1;
+
+    public Level1GUI(RecipeBook rb) {
+        setTitle("Level 1");
+        setSize(1000, 800); 
+        setLocationRelativeTo(null); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.recipebook = rb;
+        this.product = new Recipe("Product",0);
+        this.customer1 = new Customer("Alice", this.recipebook.getRecipes().get(0));
+
+        setLayout(new GridBagLayout()); 
+        btnRecipe = new JButton("Recipe Book");
+        btnIce = new JButton("Add Ice");
+        btnGin = new JButton("Add Gin");
+        btnDryVermouth = new JButton("Add Dry Vermouth");
+        btnOrangeBitters = new JButton("Add Orange Bitters");
+        btnMintLeaves = new JButton("Add Mint Leaves");
+        btnLimeWedge = new JButton("Add Lime Wedge");
+        btnSugar = new JButton("Add Sugar");
+        btnWhiteRum = new JButton("Add White Rum");
+        btnClubSoda = new JButton("Add Club Soda");
+        btnTequila = new JButton("Add Tequila");
+        btnLimeJuice = new JButton("Add Lime Juice");
+        btnTripleSec = new JButton("Add Triple Sec");
+        btnBourbonWhiskey = new JButton("Add Bourbon Whiskey");
+        btnLemonJuice = new JButton("Add Lemon Juice");
+        btnSimpleSyrup = new JButton("Add Simple Syrup");
+        btnEggWhite = new JButton("Add Egg White");
+        btnSalt = new JButton("Add Salt");
         
+        btnMuddle = new JButton("Muddle");
+        btnShake = new JButton("Shake");
+        btnStir = new JButton("Stir");
+        btnRim = new JButton("Rim");
+        btnStrain = new JButton("Strain");
+
+        btnLemonTwist = new JButton("Add Lemon Twist");
+        btnOrangeSlice = new JButton("Add Orange Slice");
+        btnCherry = new JButton("Add Cherry");
+        btnCustomer1 = new JButton(this.customer1.getName()+": "+ this.recipebook.getRecipes().get(0).getName()+" $"+ this.recipebook.getRecipes().get(0).getPrice());
+
         
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.insets = new Insets(10, 10, 10, 10); 
+        
+        constraints.gridx = 1; 
+        constraints.gridy = 0; 
+        add(btnCustomer1,constraints);
+        constraints.gridx = 0; 
+        constraints.gridy = 0; 
+        add(btnRecipe,constraints);
+        
+        JButton[] buttons = {
+            btnGin, btnDryVermouth, btnOrangeBitters, btnWhiteRum, btnTequila, 
+            btnTripleSec, btnBourbonWhiskey, btnClubSoda, btnLimeJuice, 
+            btnLemonJuice, btnSimpleSyrup, btnLimeWedge, btnMintLeaves, btnIce, btnSugar, 
+            btnEggWhite, btnSalt, btnLemonTwist, btnOrangeSlice, btnCherry, btnMuddle, 
+            btnShake, btnStir, btnRim, btnStrain
+        };
+        int buttonCount = 0;
+        int column = 2; 
+        for (JButton button : buttons) {
+            constraints.gridx = column;
+            constraints.gridy = buttonCount;
+            add(button, constraints);
+        
+            buttonCount++; 
+        
+            if (buttonCount == 10) {
+                buttonCount = 0; 
+                column++; 
+            }
+        }
+
+        this.currentProduct = new JTextArea(800, 200);
+        currentProduct.setEditable(false);
+        currentProduct.setWrapStyleWord(true);
+        currentProduct.setLineWrap(true);
+        currentProduct.setEditable(false);
+        currentProduct.setOpaque(false);
+        currentProduct.setText(product.displayProduct());
+        constraints.gridx = 0; 
+        constraints.gridy = 11;
+        constraints.gridwidth = GridBagConstraints.REMAINDER; 
+        add(currentProduct,constraints);
+
+        this.aimArea = new JTextArea(200, 200);
+        aimArea.setEditable(false);
+        aimArea.setWrapStyleWord(true);
+        aimArea.setLineWrap(true);
+        aimArea.setEditable(false);
+        aimArea.setOpaque(false);
+        aimArea.setText("Aim: $"+String.valueOf(this.aimMoney));
+        constraints.gridx = 0; 
+        constraints.gridy = 2;
+        add(aimArea,constraints);
+
+        this.gainArea = new JTextArea(200, 200);
+        gainArea.setEditable(false);
+        gainArea.setWrapStyleWord(true);
+        gainArea.setLineWrap(true);
+        gainArea.setEditable(false);
+        gainArea.setOpaque(false);
+        gainArea.setText("Gained: $"+String.valueOf(this.gainMoney));
+        constraints.gridx = 0; 
+        constraints.gridy = 3;
+        add(gainArea,constraints);
+
+        this.timeArea = new JTextArea(200, 200);
+        timeArea.setEditable(false);
+        timeArea.setWrapStyleWord(true);
+        timeArea.setLineWrap(true);
+        timeArea.setEditable(false);
+        timeArea.setOpaque(false);
+        
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count++;
+                timeArea.setText("Remaining Time: "+String.valueOf(levelTime-count));
+                if (count >= levelTime) {
+                    timeUp(currentLevel,recipebook);
+                    timer.stop(); 
+                }
+            }
+        });
+
+        constraints.gridx = 0; 
+        constraints.gridy = 1;
+        add(timeArea,constraints);
+        
+        setVisible(true);
+        timer.start();  
+
+
         btnCustomer1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int payment = customer1Pay(product);
-                gainMoney += payment;
-                product.clear();
+                customer1Pay(product,recipebook,currentLevel);
+                System.out.println("Consumer clicked");
+                currentProduct.setText(product.displayProduct());
             }
         });
 
         btnRecipe.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                displayRecipe(checktimes);
-                checktimes = 1;
+                displayRecipe(checktimes, recipebook);
+                checktimes ++;
             }
         });
 
-        btnIngredient1.addActionListener(new ActionListener() {
+        btnIce.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                product.addIngredient("ingredient1");
+                product.addIngredient("ice");
+                currentProduct.setText(product.displayProduct());
             }
         });
 
-        btnIngredient2.addActionListener(new ActionListener() {
+        btnGin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                product.addIngredient("ingredient2");
+                product.addIngredient("gin");
+                currentProduct.setText(product.displayProduct());
             }
         });
 
-        btnIngredient3.addActionListener(new ActionListener() {
+        btnDryVermouth.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                product.addIngredient("ingredient3");
+                product.addIngredient("dry vermouth");
+                currentProduct.setText(product.displayProduct());
             }
         });
 
-        setLayout(new FlowLayout());
-        add(btnCustomer1);
-        add(btnRecipe);
-        add(btnIngredient1);
-        add(btnIngredient2);
-        add(btnIngredient3);
+        btnOrangeBitters.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("orange bitters");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
 
-        setTitle("Level 1");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-        setVisible(true);
+        btnStir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("stir");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnLemonTwist.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("lemon twist");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnWhiteRum.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("white rum");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnTequila.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("tequila");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnTripleSec.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("triple sec");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnBourbonWhiskey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("bourbon whiskey");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnClubSoda.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("club soda");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnLimeJuice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("lime juice");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnLemonJuice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("lemon juice");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnSimpleSyrup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("simple syrup");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnLimeWedge.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("lime wedge");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnMintLeaves.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("mint leaves");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnSugar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("sugar");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnEggWhite.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("egg white");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnSalt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("salt");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnOrangeSlice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("orange slice");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnCherry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("cherry");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnMuddle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("muddle");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnShake.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("shake");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnRim.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("rim");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
+        btnStrain.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                product.addIngredient("strain");
+                currentProduct.setText(product.displayProduct());
+            }
+        });
+
     }
 
-
-    public static void main(String[] args) {
-       
+    public void displayRecipe(int checktimes, RecipeBook rb){
+        RecipeBookGUI rbg = new RecipeBookGUI(checktimes,rb,this);
     }
+
+    public void customer1Pay(Recipe product, RecipeBook rb, int currentLevel){
+        int m = customer1.compareNPay(product);
+        this.gainMoney += m;
+        this.gainArea.setText("Gained: $"+String.valueOf(this.gainMoney));
+        product.clear();
+        if (this.gainMoney>=this.aimMoney){
+            gameSucceed(currentLevel,rb);
+        }
+    }
+
+    public void gameSucceed(int currentLevel, RecipeBook rb){
+        new SucceedGUI(currentLevel,rb);
+    }
+    
+    public void gameFail(RecipeBook rb){
+        new FailGUI(rb);
+    }
+
+    public void timeUp(int currentLevel, RecipeBook rb){
+        if (gainMoney>=aimMoney){
+            gameSucceed(currentLevel,rb);
+        }
+        else{
+            gameFail(rb);
+        }
+    }
+
 }
 
