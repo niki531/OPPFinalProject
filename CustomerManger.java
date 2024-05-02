f7fcbbec871538a3629f3a13be46d8e6117ee6c6
niki531 <137993765+niki531@users.noreply.github.com>
2024-05-02 00:15:27 -0400

    Create CustomerManger.java

---
+++ CustomerManger.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public interface CustomerManager {
+    public static ArrayList<Customer> getLevelCustomers(int level, RecipeBook rb) {
+        ArrayList<Customer> customers = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+        Collections.addAll(names,
+            "James", "Mary", "John", "Patricia", "Alice", "Jennifer", "Michael", "Sam",
+            "William", "Elizabeth", "David", "Barbara", "Richard", "Amy", "Joseph", "Jessica",
+            "Tommy", "Sarah", "Charles", "Karen", "Christopher", "Nancy", "Daniel", "Lisa",
+            "Matthew", "Nicole", "Anthony", "Betty", "Mark", "Sandra", "Steven", "Ashley",
+            "Paul", "Dorothy", "Andrew", "Catherine", "Joshua", "Emily", "Kevin", "Donna",
+            "Brian", "Michelle", "George", "Carol", "Edward", "Amanda", "Ronald", "Melissa",
+            "Mr. Aljabbouli", "Mr. Khamse"
+        );
+        Collections.shuffle(names);
+
+        switch (level) {
+            case 1:
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                break;
+            case 2:
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                break;
+            case 3:
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(2)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(2)));
+                break;
+            case 4:
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(2)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(2)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(3)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(3)));
+                break;
+            case 5:
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(2)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(3)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(3)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(4)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(4)));
+                break;
+            case 6:
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(0)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(1)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(2)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(3)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(4)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(4)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(5)));
+                customers.add(new Customer(names.remove(0), rb.getRecipes().get(5)));
+                break;
+        }
+
+        return customers;
+    }
+    
+} 
