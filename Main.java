0310e91360a89a925beddfa4ab97e878f978021c
niki531 <137993765+niki531@users.noreply.github.com>
2024-05-02 02:52:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main{
             public void run() {
                 RecipeBook rb = RecipeBook.getInstance();
                 rb.initializeRecipeBook("recipes.txt");
-                new MainGUI(); 
+                MainGUI.getInstance().displayMain();
             }
         });
     }
