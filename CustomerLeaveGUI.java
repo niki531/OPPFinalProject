e9878bf4834e491a8b31170b21cce1f7d4f765b5
1715357606 <70891004+1715357606@users.noreply.github.com>
2024-05-02 00:01:41 -0400

    Create CustomerLeaveGUI.java

---
+++ CustomerLeaveGUI.java
@@ -0,0 +1,49 @@
+package pkg;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CustomerLeaveGUI extends JDialog {
+    private JTextArea txtMessage;
+    private JButton btnExit;
+    private boolean customerSatisfied;
+
+    public CustomerLeaveGUI(Frame owner, int customerPay, boolean modal) {
+        super(owner, modal); 
+        if (customerPay == 0) {
+            customerSatisfied = false;
+        } else {
+            customerSatisfied = true;
+        }
+        setTitle("Customer Feedback");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        txtMessage = new JTextArea();
+        txtMessage.setText(customerSatisfied ?
+            "Customer pays the bill and leaves you some tips!" :
+            "Sorry, customer is not satisfied with your drink and leaves without paying...");
+        txtMessage.setEditable(false);
+        txtMessage.setMargin(new Insets(10, 30, 10, 30));
+        add(txtMessage, BorderLayout.CENTER);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        btnExit = new JButton("Exit");
+        btnExit.addActionListener(this::exitClicked);
+        panel.add(btnExit);
+
+        add(panel, BorderLayout.SOUTH);
+        pack(); 
+        setVisible(true);
+    }
+
+    private void exitClicked(ActionEvent e) {
+        dispose();
+    }
+}
